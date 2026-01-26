fcb33a35d41ee61fd8627df372ae777f2cdd5678
Lawrence Sanzogni <91920685+Lawsan92@users.noreply.github.com>
2026-01-25 22:04:03 -0600

    Time: 14 ms (86.04%), Space: 93.4 MB (53.9%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Number, Boolean> numberMap = new HashMap<>();
-        for (Number num: nums) {
-            if (numberMap.containsKey(num)) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num: nums) {
+            if (numSet.contains(num)) {
                 return true;
             }
-            numberMap.put(num, true);
+            numSet.add(num);
         }
         return false;
     }
