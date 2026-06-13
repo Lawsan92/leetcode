c39c4931a47c4a306905024116844a69248da37d
Lawrence Sanzogni <91920685+Lawsan92@users.noreply.github.com>
2026-06-13 10:35:26 -0700

    Time: 0 ms (100%), Space: 42.3 MB (15.92%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,35 +1,17 @@
 class Solution {
     public int fib(int n) {
-        // base case
+        
         if (n == 0 || n == 1) {
             return n;
         }
-        // return fib(n - 1) + fib(n-2);
-        // f(3) = f(2) + f(1) => 1 + 1 => 2
-        int i = 1; // f(1)
-        int j = 0; // f(0)
-        int sum = i + j; // f(2): 1
-        while (n > 2) { // 3
-            sum = sum + i; // f(3): 1 + 1 => 2 | f(5): 3 + 2 => 5
-            i = sum - i;  // f(2): 2 - 1 => 1 | f(4): 5 - 2 => 3
+        int i = 1;
+        int j = 0;
+        int sum = i + j;
+        while (n > 2) {
+            sum = sum + i;
+            i = sum - i;
             n--;
         }
         return sum;
     }
 }
-
-/*
-
-f(1) = 1
-
-f(2) = f(1) + f(0) => 1 + 0 => 1
-
-f(3) = f(2) + f(1) => 1 + 1 => 2
-
-f(4) = f(3) + f(2) => 2 + 1 => 3
-
-f(5) = f(4) + f(3) => 3 + 2 => 5
-
-f(6) = f(5) + f(4) => 5 + 3 => 8
-
- */
